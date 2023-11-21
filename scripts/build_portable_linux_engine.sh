c36c5e0d8b056a008d9333aae48c58aa6d10ff6c
hasandramali <93338836+hasandramali@users.noreply.github.com>
2023-11-21 23:04:37 +0200

    Update build_portable_linux_engine.sh

--- scripts/build_portable_linux_engine.sh
+++ scripts/build_portable_linux_engine.sh
@@ -7,5 +7,4 @@ cmake \
 	-DXASH_DEDICATED=ON \
 	-DXASH_VGUI=ON \
 	-DCMAKE_BUILD_TYPE=RelWithDebInfo ../
-make -j2 VERBOSE=1
-cp engine/xash3d mainui/libxashmenu.so vgui_support/libvgui_support.so vgui_support/vgui.so ../scripts/xash3d.sh .
+make
