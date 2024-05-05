d0bda4dd6eb309114d6268e691513b5a707e96ac
hasandramali <93338836+hasandramali@users.noreply.github.com>
2024-05-05 13:39:11 +0300

    Update client.h

--- engine/client/client.h
+++ engine/client/client.h
@@ -534,9 +534,16 @@ typedef struct
 	qboolean internetservers_pending;	// internetservers is waiting for dns request
 } client_static_t;
 
-typedef struct _Queue Queue;
+typedef struct _Queue
+{
+    int count;
+    int maxelements;
+    float m_flDuration;
+    Element *first;
+    Element *last;
+    Element *current;
+}Queue;
 extern Queue* p_g_ShellQueue;
-
 
 	// download info
 extern int		downloadcount;
